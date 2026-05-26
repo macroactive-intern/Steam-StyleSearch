1c475ffedae853572e489d70b859b147b9984758
Nathan McCormick <mccormicknathan28@gmail.com>
2026-05-27 11:53:22 +1200

    Use global timers in debounce hook

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -4,9 +4,9 @@ export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    const timeout = window.setTimeout(() => setDebouncedValue(value), delay);
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
 
-    return () => window.clearTimeout(timeout);
+    return () => clearTimeout(timeout);
   }, [value, delay]);
 
   return debouncedValue;
